22b1e4350716b95a08af523cb0ed2fe50dad8f12
Marlus Araujo <m@marlus.com>
2020-07-28 21:16:13 -0300

    pages

---
+++ src/store-pages.js
@@ -0,0 +1,61 @@
+export default {
+  "404": {
+    title: "Oooops...",
+    text: "Página não encontrada ou em construção",
+    fulltext: "Página não encontrada ou em construção"
+  },
+  projeto: {
+    title: "O Projeto",
+    text: "Sinos",
+    fulltext: `<strong>Um Novo Olhar: arte e capacitação de professores e regentes para a inclusão</strong>
+
+    O Um Novo Olhar foi desenvolvido em parceria pela Fundação Nacional de Artes – Funarte e a Universidade Federal do Rio de Janeiro (UFRJ), por meio da Escola de Música da Universidade, e disponibiliza oficinas de artes em geral, com ênfase nas artes visuais, online e em várias regiões do país. O projeto reúne capacitações em arte-educação e também em regência coral. A iniciativa faz parte do Programa Funarte de Toda Gente e seu objetivo do projeto é promover a inclusão e o acesso de crianças, jovens e adultos com algum tipo de deficiência.
+    
+    O projeto tem também o objetivo de ampliar a percepção de toda a sociedade sobre as deficiências, contando para isso com a exibição online de performances de artistas e vídeo podcasts (vodcasts) sobre arte e acessibilidade; e, também com uma série de publicações. 
+      
+    <strong>Atuação em três frentes</strong>
+    
+    O projeto possui três frentes distintas, que têm a inclusão como seu principal objetivo comum:
+    
+    <strong>A primeira delas</strong>, devido às restrições impostas pela covid 19, começa apenas online e dedica-se à oferta de capacitação em arte-educação para acessibilidade, destinada especialmente a professores do ensino fundamental. As atividades começam com o Encontro Um Novo Olhar em Arte/Educação+Acessibilidade, entre os dias 5 e 6 de agosto, com palestras e mesas redondas online, no qual especialistas tratam de temas relacionados à arte-educação inclusiva. As atividades acontecem na forma de “lives”, transmitidas pelo canal Arte de Toda Gente, no Youtube, e que permanecem disponíveis aqui no no site e no canal, após sua conclusão.
+      
+    Também integra essa frente de atuação uma série de sete cursos online destinados a apoiar professores no uso das artes como ferramenta facilitadora no trato da pessoa com deficiência. Para participar, é necessário fazer uma inscrição no site, pois o acesso às aulas e ao material será dado mediante um login. Os cursos serão disponibilizados ao longo do segundo semestre de 2020 e, também, em 2021.
+      
+    <strong>A segunda frente</strong> é composta por ações de canto coral – uma das atividades musicais consideradas de maior capacidade inclusiva. Ela inclui oficinas online de capacitação para regentes de coro infantil, além de material didático – vídeos e partituras de repertório, de compositores brasileiros (especialmente formatado para esses conjuntos). E, também, conteúdo sobre acessibilidade em música para pessoas com deficiência, de forma a facilitar e tornar as apresentações mais atraentes para esse público. As oficinas são ministradas por professores da UFRJ, de outras universidades e instituições de ensino e, também, por outros especialistas em canto coral convidados. Aqui, o objetivo é proporcionar uma base de conhecimento e de recursos técnicos para que os participantes possam melhorar e ampliar sua atuação profissional.
+      
+    <strong>Já na terceira frente do projeto</strong>, que deve acontecer somente a partir do primeiro semestre do ano que vem, estão previstas oficinas presenciais de regência coral e de arte-educação para professores da rede pública, com foco na acessibilidade. Serão também promovidas oficinas de artes visuais para crianças com deficiência, em diferentes regiões do país. A ideia é incentivar o exercício da arte e trabalhar as habilidades motoras e físicas, estimulando capacidades de percepção, comunicação, expressão e sensibilidade. O desenvolvimento de todo o processo, com a evolução dos trabalhos, será registrado em audiovisual, dando origem a um documentário e um catálogo, para exibição futura.
+      
+    <strong>Vodcasts, performances e publicações</strong>
+    Paralelamente, e como apoio às demais atividades, são veiculados no site apresentações musicais e performances de artistas com deficiência, de artes visuais e cênicas e literatura, e vodcasts (vídeo podcasts) com participação de professores especialistas e outros artistas, em abordagens de temas diversos. Compõe ainda também o material de apoio a publicação de revistas sobre a acessibilidade e a arte, cadernos didáticos e partituras para canto coral – incluindo a reedição de obras para coro publicadas anteriormente pela Funarte.
+      
+    <strong>Parceria estratégica</strong>
+    Principal fomentadora, promotora e incentivadora governamental de atividades artísticas no país, a Funarte agrega, na parceria com a UFRJ, conhecimento e capacidade técnica para a execução do projeto Um Novo Olhar, no Programa Funarte de Toda Gente. Uma das principais e mais respeitadas instituições de ensino superior do país, a Universidade – com sua Escola de Música – tem em seu quadro, docentes especializados, não somente em métodos e didática específicos, direcionados a alunos, como também na capacitação de professores e regentes; e no estímulo do desenvolvimento pedagógico e artístico, por meio de oficinas, palestras e preparação de material didático. A parceria visa a promover políticas sociais públicas que proporcionem a acessibilidade e a inclusão no ambiente cultural. `
+  },
+  parceiros: {
+    title: "Parceiros",
+    text: "",
+    fulltext:`
+    <strong>QUER SER NOSSO PARCEIRO?</strong>
+
+    Que tal ter sua marca fazendo parte do <strong>Um Novo Olhar</strong>?
+
+    Entre em contato pelo email: marketing@musica.ufrj.br e saiba como.
+
+    <img src="/img/uno-parceiros.png" alt="" />
+    `
+  },
+  imprensa: {
+    title: 'Imprensa',
+    text:"",
+    fulltext: `<strong>Contatos</strong>
+
+    Assessoria de imprensa:
+    Projetos UFRJ – Funarte: imprensa@musica.ufrj.br
+    Funarte – Assessoria de Comunicação: ascomfunarte@funarte.gov.br
+    
+    Releases
+    - <a href="/downloads/release-funarte-um-novo-olhar.pdf" rel="noopener noreferrer" target="_blank">Funarte de Toda Gente</a>
+    - <a href="/downloads/release-funarte-de-toda-gente-rev-julho-2020.pdf" rel="noopener noreferrer" target="_blank">Projeto Um Novo Olhar</a>
+    `
+  }
+}
