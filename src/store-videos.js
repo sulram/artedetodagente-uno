84a25876321debf6ca5a21ba666d9e8d08be0064
Marlus Araujo <m@marlus.com>
2020-08-11 18:45:22 -0300

    remove ramda and placeholder content

--- src/store-videos.js
+++ src/store-videos.js
@@ -1,47 +1,7 @@
-import * as R from 'ramda'
-
-const videos = [
-  {
-    id: 'video-1',
-    title: 'vídeo 1',
-    text: 'um exemplo de vídeo',
-    youtube: 'https://www.youtube.com/watch?v=P0fVPrcOsq4'
-  },
-  {
-    id: 'video-2',
-    title: 'vídeo 2',
-    text: 'um exemplo de vídeo',
-    youtube: 'https://www.youtube.com/watch?v=cdZgeJ53xKk'
-  },
-  {
-    id: 'video-3',
-    title: 'vídeo 3',
-    text: 'um exemplo de vídeo',
-    youtube: 'https://www.youtube.com/watch?v=n8j7ubkXy2s'
-  },
-  {
-    id: 'video-4',
-    title: 'vídeo 4',
-    text: 'um exemplo de vídeo',
-    youtube: 'https://www.youtube.com/watch?v=-JPaHlzXwdw'
-  },
-  {
-    id: 'video-5',
-    title: 'vídeo 5',
-    text: 'um exemplo de vídeo',
-    youtube: 'https://www.youtube.com/watch?v=Qmqd6RxJ0QQ'
-  }
-]
-
 export default {
   "404": {
     title: "Oooops...",
     fulltext: "Página não encontrada ou em construção"
-  },
-  oficinasderegencia: {
-    title: "Oficinas de Regência",
-    fulltext: `Aqui você encontra os vídeos das Oficinas de Regência do Projeto Um Novo Olhar.`,
-    videos: R.clone(videos)
   },
   'repertorio-coral': {
     title: "Repertório Coral",
