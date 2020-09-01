0f9eb54e83c2fa3d0f89a86cc78c9b407ab00aca
Marlus Araujo <m@marlus.com>
2020-09-01 16:51:49 -0300

    HomeCursos title and read more

--- src/views/HomeCursos.js
+++ src/views/HomeCursos.js
@@ -7,17 +7,19 @@ import store from '../store'
 function HomeCursos(props) {
 
   const {cursos} = store.pages
+  const formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSed_X76Il411qz_isWsDT_qN8tjJNfwAaDH6CvgvNShgUT63g/viewform`
 
   return (
     <section id="cursos" className="base cursos">
       <div className="title-box divider">
-        <h2><Link to="/#cursos">Cursos Arte/Educação + Acessibilidade</Link></h2>
+        <h2><Link to="/#cursos">Cursos Arte/Educação + Acessibilidade + Inclusão</Link></h2>
       </div>
       <div className="inside-content">
         <div>{parse(cursos.fulltext)}</div>
         <div>
           <p>&nbsp;</p>
-          <a href="https://docs.google.com/forms/d/e/1FAIpQLSed_X76Il411qz_isWsDT_qN8tjJNfwAaDH6CvgvNShgUT63g/viewform" className="link-box" rel="noopener noreferrer" target="_blank">Faça sua inscrição</a>
+          <a href={formUrl} className="link-box" rel="noopener noreferrer" target="_blank">Faça sua inscrição</a>
+          &nbsp;<Link className="link-box" to="/noticias/4">Saiba mais</Link>
         </div>
       </div>
     </section>
