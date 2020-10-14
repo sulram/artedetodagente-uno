a37f17db4a032c0b5aca2fac1258afbf8f3c6bb0
caiosdf <caiosdf15@gmail.com>
2020-10-13 22:05:08 -0300

    + palestrantes congresso-internacional

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://admin.umnovoolhar.art.br/'
+    //baseURL: 'https://admin.umnovoolhar.art.br/'
+    baseURL: 'http://localhost:1339'
 })
 
 export default api
