83154a88c985e0b78fbe2439d32663843e9b3189
Marlus Araujo <m@marlus.com>
2020-11-19 15:11:44 -0300

    banner center

--- src/views/HomeBanner.js
+++ src/views/HomeBanner.js
@@ -5,7 +5,7 @@ function HomeBanner(props) {
   return (
     <section id="banner" className="banner" style={{backgroundColor: props.bgColor || '#000'}}>
       <a href={props.url} rel="noopener noreferrer" target="_blank" title={props.title}>
-        <img src={props.img} alt={props.title}/>
+        <img src={props.img} alt={props.title} style={{margin: '0 auto'}}/>
       </a>
     </section>
   );
