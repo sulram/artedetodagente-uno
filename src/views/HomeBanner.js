bb72e4ce0488d365ecfd8d4a5ac1502a45ff881d
Marlus Araujo <m@marlus.com>
2020-11-19 14:53:10 -0300

    banner: no margin

--- src/views/HomeBanner.js
+++ src/views/HomeBanner.js
@@ -3,7 +3,7 @@ import React from 'react'
 function HomeBanner(props) {
 
   return (
-    <section id="banner" className="base banner" style={{backgroundColor: props.bgColor || '#000'}}>
+    <section id="banner" className="banner" style={{backgroundColor: props.bgColor || '#000'}}>
       <a href={props.url} rel="noopener noreferrer" target="_blank" title={props.title}>
         <img src={props.img} alt={props.title}/>
       </a>
