15f86a61aa8697d5156e511bdf28481a5ab864f4
Marlus Araujo <m@marlus.com>
2020-11-24 12:16:21 -0300

    HomeCursos

--- src/views/HomeCursos.js
+++ src/views/HomeCursos.js
@@ -2,12 +2,9 @@ import React from 'react'
 import parse from 'html-react-parser'
 import { HashLink as Link } from 'react-router-hash-link'
 
-import store from '../store'
-
 function HomeCursos(props) {
 
-  const {cursos} = store.pages
-  // const formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSed_X76Il411qz_isWsDT_qN8tjJNfwAaDH6CvgvNShgUT63g/viewform`
+  const formUrl = `https://docs.google.com/forms/d/e/1FAIpQLSed_X76Il411qz_isWsDT_qN8tjJNfwAaDH6CvgvNShgUT63g/viewform`
 
   return (
     <section id="cursos" className="base cursos">
@@ -15,10 +12,12 @@ function HomeCursos(props) {
         <h2><Link to="/#cursos">Cursos Arte/Educação + Acessibilidade + Inclusão</Link></h2>
       </div>
       <div className="inside-content">
-        <div>{parse(cursos.fulltext)}</div>
         <div>
+          <p>Estão abertas as inscrições para uma nova turma do curso Arte/Educação, Tecnologia Assistiva e Deficiência, curso de capacitação para educadores oferecido pelo projeto Um Novo Olhar e que faz parte da série de cursos Arte/Educação + Acessibilidade + Inclusão.</p>
+          <p>O curso irá abordar questões relacionadas à inclusão, através das artes, de alunos com deficiência no ambiente escolar, como: o modelo de educação inclusiva, a importância da arte/educação para promover a integração de alunos com e sem deficiência e a contribuição da tecnologia assistiva como facilitadora do processo de aprendizagem ou desenvolvimento de habilidades.</p>
+          <p>As aulas começam dia 07/12 e serão realizadas no Ambiente Virtual de Aprendizagem da UFRJ.</p>
           <p>&nbsp;</p>
-          {/* <a href={formUrl} className="link-box" rel="noopener noreferrer" target="_blank">Faça sua inscrição</a> */}
+          <a href={formUrl} className="link-box" rel="noopener noreferrer" target="_blank">Faça sua inscrição</a>
           &nbsp;<Link className="link-box" to="/noticias/PANORAMA-DA-ARTE-EDUCACAO-INCLUSIVA">Saiba mais</Link>
         </div>
       </div>
